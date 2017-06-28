640d031a3cb9945258129a6ced4fcfa6bdb59ae8
YD-UOM <yid3@student.unimelb.edu.au>
2017-06-28 19:14:27 +1000

    Update mbox_parser.py

--- mbox_parser.py
+++ mbox_parser.py
@@ -2,5 +2,6 @@ import mailbox
 import csv
 
 writer = csv.writer(open("clean_mail.csv", "w"))
+writer.writerow(['subject', 'from', 'date'])
 for message in mailbox.mbox('abc.mbox'):
 	writer.writerow([message['subject'], message['from'], message['date']])
